2b0b4be743e1a626c7aee571dabbd47069848b82
Joseph Sparks <jls062015@icloud.com>
2024-02-15 08:00:36 -0500

    final updates to list.c and list.h

--- linkedLists/list.c
+++ linkedLists/list.c
@@ -17,12 +17,16 @@ Node* make_node(int data)
 
 Node* find_in_list(Node* list, int target)
 {
+    // Start traversal from the head of the list
     Node* current = list;
 
+    // Traverse the list until the end is reached
     while (current != NULL) {
+        // Check if the current node's value matches the target value
         if (current->val == target) {
             return current; // Found the target, return the node
         }
+        // Move to the next node in the list
         current = current->next;
     }
 
@@ -34,4 +38,21 @@ void insert_in_front(Node** list, Node** newnode)
 {
     (*newnode)->next = *list;
     *list = *newnode;
+}
+
+void insert_at_end(Node** list, Node** newnode) {
+    // If the list is empty, make the new node the head of the list
+    if (*list == NULL) {
+        *list = *newnode;
+    } else {
+        // Traverse the list to find the last node
+        Node* current = *list;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        // Insert the new node at the end of the list
+        current->next = *newnode;
+    }
+    // Ensure the new node points to NULL as it's the last node
+    (*newnode)->next = NULL;
 }

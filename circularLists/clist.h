238a6f65ec0c59402f33ec791cb383e9c26002f7
Joseph Sparks <jls062015@icloud.com>
2024-02-26 08:12:41 -0500

    update to fit name requirements

--- circularLists/clist.h
+++ circularLists/clist.h
@@ -6,6 +6,6 @@ typedef struct node {
 Node* make_node(int);
 Node* remove_from_clist(Node**, int);
 void insert_at_end(Node**, Node**);
-char* print_list(Node*);
+char* print_clist(Node*);
 void clist_insert_in_order(Node**, Node**);
 void clist_insert_at_index(Node**, int, int);

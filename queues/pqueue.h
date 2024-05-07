7992fab20f9dd9cb43ae41e37063c11ce42c2fb0
Joseph Sparks <jls062015@icloud.com>
2024-05-06 22:31:32 -0400

    update huffman.c

--- queues/pqueue.h
+++ queues/pqueue.h
@@ -1,8 +1,11 @@
+#include "treenode.h"
+
 typedef struct node {
-    int val;
+    TreeNode* node;
     int weight;
     struct node* next;
 } Node;
 
-Node* make_node(int val, int weight);
-void sortedAppend(Node** head, int val, int weight);
+Node* make_pq_node(char data, int weight);
+void sortedAppend(Node** head, Node* temp);
+void print_pq(Node* head);
